3ed79563a56bbab7886570e01c08c5f94f574d13
Bence Kovács <kbence55@gmail.com>
2024-05-25 12:08:56 +0300

    Remove redundant function

--- PathFinder_Plus/Services/BacktrackingService.cs
+++ PathFinder_Plus/Services/BacktrackingService.cs
@@ -31,14 +31,11 @@
             _client = new HttpClient();
         }
 
-        public async Task<Route> FindMinimumDistanceRouteBt(Coordinate start, Coordinate end, int buffer = 250)
+        public async Task<Route> FindMinimumDistanceRouteBt(Coordinate start, Coordinate end, int buffer)
         {
             var standardPois = await _poiService.GetPOIsStandard(start, end, buffer);
 
             var pois = ExtractPointsFromResponse(standardPois);
-
-            //return new Route(new List<Coordinate>(), 0);
-            //pois.Insert(0, start);
 
             var payloadPois = pois.Select(poi => new double[] { poi.Longitude, poi.Latitude }).ToList();
 
@@ -122,80 +119,6 @@
 
             return points;
         }
-
-        //public async Task<Route> FindMinimumDistanceRouteBt(List<Coordinate> pois, Coordinate start)
-        //{
-        //    pois.Insert(0, start);
-
-        //    var payloadPois = pois.Select(poi => new double[] { poi.Longitude, poi.Latitude }).ToList();
-
-        //    using StringContent jsonContent = new(
-        //     JsonSerializer.Serialize(new
-        //     {
-        //         locations = payloadPois,
-        //         metrics = new string[] { "distance" }
-        //     }),
-        //     Encoding.UTF8,
-        //     "application/json");
-
-        //    HttpRequestMessage request = new()
-        //    {
-        //        Method = HttpMethod.Post,
-        //        RequestUri = new Uri(matrixBaseUrl),
-        //        Headers = { { "Authorization", ORS_KEY } },
-        //        Content = jsonContent
-        //    };
-
-        //    using HttpResponseMessage response = await _client.SendAsync(request);
-
-        //    response.EnsureSuccessStatusCode();
-
-        //    var distanceMatrix = new double[pois.Count][];
-
-        //    try
-        //    {
-        //        var jsonResponse = await response.Content.ReadAsStringAsync();
-        //        var jsonObject = JsonSerializer.Deserialize<DistanceMatrix>(jsonResponse);
-
-        //        if (jsonObject != null && jsonObject.distances != null)
-        //        {
-        //            distanceMatrix = jsonObject.distances;
-        //            for (int i = 0; i < distanceMatrix.GetLength(0); i++)
-        //            {
-        //                for (int j = 0; j < distanceMatrix.GetLength(0); j++)
-        //                {
-        //                    Console.Write($"{distanceMatrix[i][j]} ");
-        //                }
-        //                Console.WriteLine();
-        //            }
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("Deserialization resulted in null object.");
-        //        }
-        //    }
-        //    catch (JsonException ex)
-        //    {
-        //        Console.WriteLine("Error deserializing JSON: " + ex.Message);
-        //    }
-
-        //    var permutations = GetPermutationsBt(pois);
-        //    Route minRoute = null;
-        //    double minDistance = double.MaxValue;
-
-        //    foreach (var permutation in permutations)
-        //    {
-        //        var totalDistance = CalculateRouteDistance(permutation, distanceMatrix);
-        //        if (totalDistance < minDistance)
-        //        {
-        //            minDistance = totalDistance;
-        //            minRoute = new Route(permutation, totalDistance);
-        //        }
-        //    }
-
-        //    return minRoute;
-        //}
-
         private static double CalculateRouteDistance(List<Coordinate> route, double[][] distanceMatrix)
         {
             var totalDistance = 0.0;
